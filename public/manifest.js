f53fbf7058ef2be6a7c0f7e1e754c8245858e460
Giorgi <vasokima@gmail.com>
2025-05-28 15:15:38 +0400

    Create manifest.js

---
+++ public/manifest.js
@@ -0,0 +1,23 @@
+{
+    "name": "კრიპტო კოლექტივი X",
+    "short_name": "Crypto Collective X",
+    "description": "Track cryptocurrency prices, fear & greed index, and market trends.",
+    "start_url": "https://ccx5.vercel.app/",
+    "display": "standalone",
+    "background_color": "#1A1A2E",
+    "theme_color": "#1A1A2E",
+    "orientation": "portrait",
+    "icons": [
+        {
+            "src": "https://ta3bviqjtxy1kv7x.public.blob.vercel-storage.com/bgc-wAyOl9axJJUu4bUqug06dN3eSLPzt4.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "https://ta3bviqjtxy1kv7x.public.blob.vercel-storage.com/bgc-wAyOl9axJJUu4bUqug06dN3eSLPzt4.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ],
+    "lang": "ka"
+}
